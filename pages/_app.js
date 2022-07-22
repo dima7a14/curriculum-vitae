812ef77cd391abf6cd0e11c8879162733b0d94b3
Dima Danyliuk <dimadanylyuk@gmail.com>
2022-07-22 20:31:07 +0300

    Add transition between pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { LazyMotion, m, domAnimation, AnimatePresence } from 'framer-motion';
 
 import Layout from '../components/layout';
 import '../styles/globals.css';
 
-function CVApp({ Component, pageProps }) {
+const animation = {
+	variants: {
+		initial: {
+			opacity: 0,
+			top: '100vh',
+			scale: 0.4,
+		},
+		animate: {
+			opacity: 1,
+			top: '0vh',
+			scale: 1,
+		},
+		exit: {
+			opacity: 0,
+			top: '100vh',
+			scale: 0.4,
+		},
+	},
+	transition: {
+		duration: 0.4,
+	},
+};
+
+function CVApp({ Component, pageProps, router }) {
 	return (
 		<Layout>
-			<Component {...pageProps} />
+			<LazyMotion features={domAnimation}>
+				<AnimatePresence exitBeforeEnter>
+					<m.div
+						key={router.route}
+						variants={animation.variants}
+						transition={animation.transition}
+						initial="initial"
+						animate="animate"
+						exit="exit"
+					>
+						<Component {...pageProps} />
+					</m.div>
+				</AnimatePresence>
+			</LazyMotion>
 		</Layout>
 	);
 }
@@ -15,6 +52,9 @@ function CVApp({ Component, pageProps }) {
 CVApp.propTypes = {
 	Component: PropTypes.func.isRequired,
 	pageProps: PropTypes.object.isRequired,
+	router: PropTypes.shape({
+		route: PropTypes.string.isRequired,
+	}).isRequired,
 };
 
 export default CVApp;
